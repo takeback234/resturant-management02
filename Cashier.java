0d708e7e3d21a53b2e46f758992fe7c9265abee8
takeback234 <62907000+takeback234@users.noreply.github.com>
2020-04-08 20:53:33 +0600

    Add files via upload

--- Cashier.java
+++ Cashier.java
@@ -1,35 +1,61 @@
 
-package resturantsmanagemento02;
-
+package resturant.management;
 
 public class Cashier extends Employee {
+    
+    int terminalno;
+    int sesssionno;
+    Order order;
 
-    public Cashier(String id, String name, float salary, boolean active) {
-        super(id, name, salary, active);
+    public Cashier(int terminalno, int sesssionno, int id, String firstname, String lastname, String email, Department department, String position, Address adress, Efficiency efficiency) {
+        super(id, firstname, lastname, email, department, position, adress, efficiency);
+        this.terminalno = terminalno;
+        this.sesssionno = sesssionno;
+    }
+    
+    public void takeorder(Order order)
+    {
+        System.out.println("Order id:"+order.orderid);
+        System.out.println("Customer id:"+order.customerid);
+    }
+    public double calculation(int quantity,double perprice )
+    {
+        double price=quantity*perprice;
+        return price;
+    }
+    
+    public void bill(double price)
+    {
+        if(price>1000)
+        {
+           double discount=price*(10.0/100);
+           double Price=price-discount;
+            System.out.println("your bill is="+Price);
+        }
+        else if(price>2000)
+        {
+            double discount=price*(15.0/100);
+            double Price=price-discount;
+            System.out.println("your bill is="+Price);
+        }
     }
 
-    public Cashier(String id, String name, float salary) {
-        super(id, name, salary);
-    }
-   public void takeorder(String s)
-   {
-       System.out.println(s+"has been taken");
-   } 
-   public boolean orderfinished (boolean a)
-   {
-      return a;
-   }
-   
 
     @Override
-    public void display() {
-        System.out.println("display");
+    public void checkednotification() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void payment() {
-        System.out.println("payment"+super.getSalary());
+    public void login() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public void logout() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
     
 }
